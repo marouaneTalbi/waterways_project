e0f81c05849aa2dc8c7b13d06989ebde62e0a398
marouaneTalbi <talbi.918marouane@gmail.com>
2023-11-04 20:34:26 +0100

    envoi de mail marche pas

--- back/src/Controller/RegisterController.php
+++ back/src/Controller/RegisterController.php
@@ -3,39 +3,63 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Services\MailerService;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route(path: '/api/register', name: 'api_register', methods: ['POST'] )]
+    private $mailer;
 
-    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder, EntityManagerInterface $entityManager): Response
+    public function __construct(MailerInterface $mailer)
     {
-        $requestData = json_decode($request->getContent(), true);
-        $email = $requestData['email']; // Remplacez par le nom d'utilisateur
-        $plainPassword = $requestData['password']; // Remplacez par le mot de passe
+         $this->mailer = $mailer;
+    }
+
+
+
+
+    #[Route(path: '/api/register', name: 'api_register', methods: ['POST'] )]
+    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('marwane.berkani@gmail.com')
+            ->to('marwane.berkani@gmail.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
+        // $requestData = json_decode($request->getContent(), true);
+        // $email = $requestData['email']; // Remplacez par le nom d'utilisateur
+        //  $plainPassword = $requestData['password']; // Remplacez par le mot de passe
         // $firstname = $requestData['firstname']; // Remplacez par le mot de passe
         // $lastname = $requestData['lastname']; // Remplacez par le mot de passe
-        $token = $requestData['token']; // Remplacez par le mot de passe
+        //  $token = $requestData['token']; // Remplacez par le mot de passe
 
 
-        $user = new User();
-        $user->setEmail($email);
-        $user->setToken($token);
+        //  $user = new User();
+        //   $user->setEmail($email);
+        //    $user->setToken($token);
 
-        $user->setPassword($passwordEncoder->hashPassword(
-            $user,
-            $plainPassword
-        ));
+        //   $user->setPassword($passwordEncoder->hashPassword(
+        //       $user,
+        //       $plainPassword
+        //    ));
 
-        $entityManager->persist($user);
-        $entityManager->flush();
+        // $entityManager->persist($user);
+        //  $entityManager->flush();
 
         return new Response('Utilisateur enregistré avec succès',Response::HTTP_OK);
     }
+
+    
 }
