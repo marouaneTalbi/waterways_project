df6b17911e149a9cc8df523a7185c89c1860bafe
marouaneTalbi <talbi.918marouane@gmail.com>
2023-11-06 23:45:09 +0100

    tester le login et le register

--- front/src/pages/Search/search.js
+++ front/src/pages/Search/search.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import LoginForm from "../Login/login";
+import Register from "../Register/register";
 export default function search() {
+
   return (
     <>
         <div>search</div>
+        <LoginForm />
+
+
     </>
   )
 }
