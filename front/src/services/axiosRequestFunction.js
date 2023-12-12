c6d1f02c5a98a42883809e52f95fa2ec1afcc2a3
marouaneTalbi <talbi.918marouane@gmail.com>
2023-12-12 22:27:22 +0100

    on peut modifier un etablissement et le refresh token marche

--- front/src/services/axiosRequestFunction.js
+++ front/src/services/axiosRequestFunction.js
@@ -27,53 +27,68 @@ export function getUserRole() {
 }
 
 const sendRequest = async (endpoint, method = 'GET', data = {}, requireAuth = true) => {
-  if (requireAuth) {
+  const isValidToken = !isTokenExpired();
+
+  if (requireAuth && isValidToken) {
     const token = localStorage.getItem('token');
     if (token) {
       axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     } else {
       delete axiosInstance.defaults.headers.common['Authorization'];
     }
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method,
+        data,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error with the request:', error.response?.data || error.message);
+      throw error;
+    }
+  } else {
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method,
+        data,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error with the request:', error.response?.data || error.message);
+      throw error;
+    }
   }
+}
 
-  try {
-    const response = await axiosInstance({
-      url: endpoint,
-      method,
-      data,
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error with the request:', error.response?.data || error.message);
-    throw error;
-  }
-};
-
+ 
 axiosInstance.interceptors.request.use(async config => {
-    if (config.url.includes('/auth')
-        || config.url.includes('/api/token/refresh')
-        || (config.url.includes('/api/users') && config.method === 'post')
-        || (config.url.includes('/api/mdpresetemail') && config.method === 'post')
-        || (config.url.includes('/api/resetmdp') && config.method === 'post')
-    ) {
-      return config;
-    }
 
-    const token = localStorage.getItem('token');
-    const isValidToken = !isTokenExpired();
+  const token = localStorage.getItem('token');
+  const isValidToken = !isTokenExpired();
 
-    if (!isValidToken) {
-      const newToken = await refreshToken();
-      console.log('newToken', newToken)
-      config.headers['Authorization'] = `Bearer ${newToken}`;
-    } else {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
 
+  if (config.url.includes('/auth')
+      || config.url.includes('/api/token/refresh')
+      || (config.url.includes('/api/users') && config.method === 'post')
+      || (config.url.includes('/api/mdpresetemail') && config.method === 'post')
+      || (config.url.includes('/api/resetmdp') && config.method === 'post')
+  ) {
     return config;
-  }, error => {
-    return Promise.reject(error);
-  });
+  }
+
+  if (!isValidToken) {
+    const newToken = await refreshToken();
+    config.headers['Authorization'] = `Bearer ${newToken}`;
+  } else {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 
 export  function isTokenExpired() {
   if(!localStorage.getItem('token')) {
@@ -89,15 +104,17 @@ export  function isTokenExpired() {
 }
 
 async function refreshToken() {
-  // try {
+  try {
     const refresh_token = localStorage.getItem('refresh_token');
     const response = await sendRequest('/api/token/refresh ','post', { refresh_token }, false);
     localStorage.setItem('token', response.token);
-    console.lor(response)
-    return response.token;
-  // } catch (error) {
-  //   console.log(error)
-  // }
+    if(response) {
+      window.location.reload();
+      return response.token;
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default sendRequest;
