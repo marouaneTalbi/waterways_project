741bdcdb763cd1d369d358c05d082b09ecf4db86
marouaneTalbi <talbi.918marouane@gmail.com>
2023-12-24 13:26:23 +0100

    tester la cd front

--- front/src/layouts/Header/header.js
+++ front/src/layouts/Header/header.js
@@ -46,7 +46,7 @@ export default function Header() {
                 </Dropdown>
                 <Navbar.Toggle />
             </div>
-            <Navbar.Collapse> deployment test 13
+            <Navbar.Collapse> deployment test 14
             {
                     !userRole || isValidToken && (
                         <>
