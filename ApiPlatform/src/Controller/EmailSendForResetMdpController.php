add7e25ebf429c97e708ed675c2db765ce4c6e6f
marouaneTalbi <talbi.918marouane@gmail.com>
2023-12-24 22:28:41 +0100

    deploiment back test

--- ApiPlatform/src/Controller/EmailSendForResetMdpController.php
+++ ApiPlatform/src/Controller/EmailSendForResetMdpController.php
@@ -7,7 +7,7 @@ use App\State\UserPasswordHasher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Entity\User;   
+use App\Entity\User;     
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
