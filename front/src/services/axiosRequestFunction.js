860f09d20483d8732bf4fd62eab406d73bbd201c
amine <aminecheriguifr@gmail.com>
2024-02-06 22:11:01 +0100

    fix bug upload Kbis

--- front/src/services/axiosRequestFunction.js
+++ front/src/services/axiosRequestFunction.js
@@ -5,10 +5,6 @@ import { useNavigate, Params } from 'react-router';
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
   baseURL: API_BASE_URL
 }); 
 
