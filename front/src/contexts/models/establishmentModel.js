f81b51a54d5d03c1d8eca13ea5a6b5cdd7af64c1
marouaneTalbi <talbi.918marouane@gmail.com>
2024-02-11 21:35:26 +0100

    afficher mes etablissements

--- front/src/contexts/models/establishmentModel.js
+++ front/src/contexts/models/establishmentModel.js
@@ -1,9 +1,6 @@
 import sendRequest from "../../services/axiosRequestFunction"
 
 const establishmentApi = {
-    getList: async function () {
-        return sendRequest(`/api/establishments`, 'get', {}, true);
-    },
     add: async function ({name, address, city, startDate, endDate}) {
         return sendRequest('/api/addestablishment','post',{name, address, city, startDate, endDate},true)
     },
@@ -12,7 +9,10 @@ const establishmentApi = {
     },
     edit: async function ({name, address, city, startDate, endDate}, id) {
         return sendRequest('/api/establishments/'+id,'put',{name, address, city, startDate, endDate},true)
-    }
+    },
+    getList: async function (id) {
+        return sendRequest(`/api/establishment/${id}/user`, 'get', {}, true);
+    },
 }
 
 export default establishmentApi;
