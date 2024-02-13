d25a42e06fdbacf399eed4cfca0439cc4f2d24b5
amine <aminecheriguifr@gmail.com>
2024-02-13 10:54:33 +0100

    bug roles

--- ApiPlatform/migrations/Version20240212114643.php
+++ ApiPlatform/migrations/Version20240212114643.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240210183115 extends AbstractMigration
+final class Version20240212114643 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,13 @@ final class Version20240210183115 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE boat ADD price DOUBLE PRECISION DEFAULT NULL');
+        //$this->addSql('DROP INDEX unique_user_boat');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE boat DROP price');
+       // $this->addSql('CREATE SCHEMA public');
+        //$this->addSql('CREATE UNIQUE INDEX unique_user_boat ON note (createdby_id, boat_id)');
     }
 }
