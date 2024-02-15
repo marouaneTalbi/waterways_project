469ce7797feecf10fad270346961d6a072c3639c
TheoBourdel <bourdeltheo@gmail.com>
2024-02-15 10:39:36 +0100

    fix(resa): add resa css + fix resa

--- front/src/components/Reservation/ReservationList.js
+++ front/src/components/Reservation/ReservationList.js
@@ -1,31 +1,32 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ReservationContext } from '../../contexts/reservationContext';
 import ReservationItem from './ReservationItem';
 
 export default function ReservationList() {
     const { reservationList, getReservationList } = useContext(ReservationContext);
-    const [uniqueReservationIds, setUniqueReservationIds] = useState(new Set());
 
     useEffect(() => {
         getReservationList();
     }, []);
 
-    useEffect(() => {
-        const uniqueIds = new Set(reservationList.map(reservation => reservation.id));
-        setUniqueReservationIds(uniqueIds);
-    }, [reservationList]);
 
     return (
         <>
-            <span className='text-gray-500 text-sm mb-4'>
-                {uniqueReservationIds.size} {uniqueReservationIds.size > 1 ? 'réservations trouvées' : 'réservation trouvée'}
+            <span className='text-gray-500 text-sm mb-4 w-full'>
+                {
+                    reservationList && (
+                        reservationList.length ? `${reservationList.length} réservations trouvées` : `${reservationList.length} réservation trouvée`
+                    )
+                }
+                {/* {uniqueReservationIds.size} {uniqueReservationIds.size > 1 ? 'réservations trouvées' : 'réservation trouvée'} */}
             </span>
-            <div className="overflow-x-auto">
-                <div className='mt-4 h-[200px]'>
-                    {Array.from(uniqueReservationIds).map(reservationId => {
-                        const reservation = reservationList.find(reservation => reservation.id === reservationId);
-                        return <ReservationItem key={reservationId} reservation={reservation} />;
-                    })}
+            <div className="overflow-x-auto w-full">
+                <div className='h-[400px] w-full'>
+                    {
+                        reservationList && reservationList.map((reservation) => {
+                            return <ReservationItem key={reservation.id} reservation={reservation} />
+                        })
+                    }
                 </div>
             </div>
         </>
